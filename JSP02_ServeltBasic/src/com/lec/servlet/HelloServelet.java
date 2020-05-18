09280b2000aeba58c74bdfc59c55df65399add2b
writerkang <goodwriterkang@gmail.com>
2020-05-18 17:56:49 +0900

    200518 수업종료

---
+++ JSP02_ServeltBasic/src/com/lec/servlet/HelloServelet.java
@@ -0,0 +1,60 @@
+package com.lec.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServelet
+ */
+//@WebServlet("/He")
+public class HelloServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServelet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("안녕하세요 첫번째 Servlet입니다.");
+		
+//		서블릿으로 HTML 문서 response하기
+		//1. content type 설정
+		//2. PrintWriter 객체생성 <-- response 객첼부터
+		//3. PrintWriter 객체로 HTML 스트림에 쓰기
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html lang='ko>");
+		out.println("<head>");
+		out.println("<meta charset='utf-8'>");
+		out.println("<title>서블릿 response</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>HTML문서 response</h2>");		
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
