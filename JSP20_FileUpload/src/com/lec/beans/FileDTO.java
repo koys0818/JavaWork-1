9caefeafaf3d9f7a0eeacd22e65d093b72c0b60a
writerkang <goodwriterkang@gmail.com>
2020-06-02 15:33:49 +0900

    200622

---
+++ JSP20_FileUpload/src/com/lec/beans/FileDTO.java
@@ -0,0 +1,59 @@
+package com.lec.beans;
+
+public class FileDTO {
+	private int uid; //bf_uid
+	private String source; //bf_source
+	private String file; // bf_file
+	private boolean isImage; //이미지 여부
+	
+	//생성자, getter/setter
+	public FileDTO(int uid, String source, String file) {
+		super();
+		this.uid = uid;
+		this.source = source;
+		this.file = file;
+	}
+
+	public FileDTO() {
+		super();
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getFile() {
+		return file;
+	}
+
+	public void setFile(String file) {
+		this.file = file;
+	}
+
+	public boolean isImage() {
+		return isImage;
+	}
+
+	public void setImage(boolean isImage) {
+		this.isImage = isImage;
+	}
+	
+	
+	
+	
+	
+	
+
+}
